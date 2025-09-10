9ffde1f434de3e1512ea2b388ec60f6ecdc12952
Bhavesh <bhavesh252004@gmail.com>
2025-09-10 15:25:04 +0530

    Fix backendUrl and remove extra space issue in DonationPage

--- server/server.js
+++ server/server.js
@@ -17,16 +17,9 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://bhaveshkrpanjwani.github.io'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-app.options('*', cors());
+app.use(cors());  // already applies to all routes
+app.options(/.*/, cors());
+
 
 // Define Routes
 app.use('/api/users', userRoutes);
